b1450dd0e9fea701066cf0f7fb1d6c9ee1b23865
Tom Arbuthnot <tom@tomarbuthnot.com>
2016-01-12 12:27:57 +0000

    Added MSI

--- Get-SfBClientVersion.ps1
+++ Get-SfBClientVersion.ps1
@@ -1,26 +1,31 @@
 # Currently works for Office 2016 x86 in the default program files path
 
+
+$2016CR2x86Test = Test-Path ("${Env:ProgramFiles(x86)}" + "\Microsoft Office\root\Office16\lync.exe")
+
+IF ($2016CR2x86Test -eq $true)
+{
 $SFBEXE = Get-Item ("${Env:ProgramFiles(x86)}" + "\Microsoft Office\root\Office16\lync.exe")
 $SfBMSO = Get-Item ("${Env:ProgramFiles(x86)}" + "\Microsoft Office\root\Office16\MSOSB.DLL")
+$InstallType = "2016 x86 C2R"
+}
+
+$2016MSIx86Test = Test-Path ("${Env:ProgramFiles(x86)}" + "\Microsoft Office\Office16\lync.exe")
+
+IF ($2016MSIx86Test -eq $true)
+{
+$SFBEXE = Get-Item ("${Env:ProgramFiles(x86)}" + "\Microsoft Office\Office16\lync.exe")
+$SfBMSO = Get-Item ("${Env:ProgramFiles(x86)}" + "\Microsoft Office\Office16\MSOSB.DLL")
+$InstallType = "2016 x86 MSI"
+}
+
+
 
 $sfbexeVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($SFBEXE)
 $sfbmsoVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($SfBMSO)
 
 Write-Host " "
+Write-Host "$InstallType"
 Write-Host "SFB EXE Version: $($sfbexeVersion.fileversion)"
 Write-Host "SFB MSO Version: $($sfbmsoVersion.fileversion)"
 
-<#
-# If Sigcheck exists check timestamps
-
-$SigCheck = "C:\Dropbox\PowerShell\SysInternals\sigcheck.exe"
-# Windows SysInternals Sigcheck https://technet.microsoft.com/en-gb/sysinternals/bb897441.aspx
-
-
-IF ((Test-Path $SigCheck) -eq $True)
-    {
-    Write-Host "SigCheck Found"
-    $SfBexeSigcheck = Start-Process -FilePath $SigCheck -ArgumentList "'C:\Program Files (x86)\Microsoft Office\root\Office16\lync.exe'" -WindowStyle Normal | Out-String
-
-    }
-#>
