6dbfa33e26e5bafeb5b319987091f7d77d43f1d6
Tom Arbuthnot <tomarbuthnot@users.noreply.github.com>
2016-04-08 11:53:12 +0100

    Update Get-SfBClientVersion.ps1

--- Get-SfBClientVersion.ps1
+++ Get-SfBClientVersion.ps1
@@ -8,7 +8,10 @@
 $ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/tomarbuthnot/Get-SfBClientVersion/master/Get-SfBClientVersion.ps1
 Invoke-Expression $($ScriptFromGithHub.Content)
 
+
 #>
+
+##################### Office 2016 ################################################
 
 # Office 2016 x86 C2R
 
@@ -35,7 +38,30 @@ $SfBUCCAPI = Get-Item ("${Env:ProgramFiles(x86)}" + "\Microsoft Office\Office16\
 $InstallType = "Office 2016 x86 MSI"
 }
 
+# Office 2016 x64 C2R
 
+$2016CR2x64Test = Test-Path ("${Env:ProgramFiles}" + "\Microsoft Office\root\Office16\lync.exe")
+
+IF ($2016CR2x64Test -eq $true)
+{
+$SFBEXE = Get-Item ("${Env:ProgramFiles}" + "\Microsoft Office\root\Office16\lync.exe")
+$SfBMSO = Get-Item ("${Env:ProgramFiles}" + "\Microsoft Office\root\Office16\MSOSB.DLL")
+$SfBUCCAPI = Get-Item ("${Env:ProgramFiles}" + "\Microsoft Office\root\Office16\UccApi.dll")
+$InstallType = "Office 2016 x64 C2R"
+}
+
+# Office 2016 x64 MSI
+
+
+$2016MSIx64Test = Test-Path ("${Env:ProgramFiles(x86)}" + "\Microsoft Office\Office16\lync.exe")
+
+IF ($2016MSIx64Test -eq $true)
+{
+$SFBEXE = Get-Item ("${Env:ProgramFiles}" + "\Microsoft Office\Office16\lync.exe")
+$SfBMSO = Get-Item ("${Env:ProgramFiles}" + "\Microsoft Office\Office16\MSOSB.DLL")
+$SfBUCCAPI = Get-Item ("${Env:ProgramFiles}" + "\Microsoft Office\Office16\UccApi.dll")
+$InstallType = "Office 2016 x64 MSI"
+}
 
 
 
